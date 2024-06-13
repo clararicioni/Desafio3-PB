582d90e66782016447823d5df73e4b0a63bc94bb
Clara Ricioni <bergamoclara9@gmail.com>
2024-06-13 15:36:49 -0300

    feat: quality section

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/NavBar'
 import Footer from '../components/Footer'
+import Quality from '../components/Quality'
 
 const Shop = () => {
   return (
     <div>
       <Navbar />
       <p>Shop</p>
+      <Quality />
       <Footer />
     </div>
   )
